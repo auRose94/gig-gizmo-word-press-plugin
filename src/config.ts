25d2031b98e1e67acea05d0fb0cf42ae3d01c8da
Cory Null(Noll) Crimmins - Golden <cory190@live.com>
2019-03-09 01:40:08 -0700

    0.4.11!

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 
-export const dev = true;
-export const port = dev ? 3000 : 443;
+export const dev = false;
+export const port = dev ? 58000 : 443;
 export const server = dev ? "localhost" : "giggizmo.com";
